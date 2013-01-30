f1c5bb2c2b8b7873c2a48baecb0fd2c4efb2bd9e
Kevin-Bravo <bravo@cmu.edu>
2013-01-30 00:44:13 -0500

    added some formatting stuff, thought really hard about certain things

--- lab1/src/com/cs440/lab1/classes/ProcessManager.java
+++ lab1/src/com/cs440/lab1/classes/ProcessManager.java
@@ -1,5 +1,15 @@
-import java.util.*
+//package com.cs440.lab1.classes;
+import java.util.*;
+//import java.io.FileInputStream;
+import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.Serializable;
 
+class ProcessTable {
+	/*holds a slave's process, and that process's time on this processor*/
+	private int time = 0;
+	String hostname;
+}
 
 class SlaveHost {
 	/*contains a slave to run on, and a process*/
@@ -8,32 +18,42 @@ class SlaveHost {
 	  hash currently used process id's so
 	  we can efficiently create new processes
 	*/
-	private List<int> process_id;
+	private List<Integer> process_id;
 	public String getHostName() {
 		return hostname;
 	}
 }
 
-public static class ProcessManager {
+public class ProcessManager {
 	List<SlaveHost> slave_list;
 	boolean master;
 
-	ProcessManager() {
-		
+	ProcessManager() {	
 
 	}
 
-        void process_input(byte[]
-	public static void main() {
+        //void process_input(byte[]
+	public static void main(String[] argv) throws IOException {
 		int b;
 		byte[] input;
-		int length  = 0;
+		long length  = 0;
 		long offset = 0;
-		while(1) {
+		if (argv.length == 2) {
+			if (argv[0] != "-c") {
+				System.err.println("ERROR: Bad input");
+			}
+			//connect to the master
+		}
+		else if (argv.length != 0) {
+			System.err.println("ERROR: Incorrect # of arguments");
+		}
+
+
+		while(true) {
 			b = System.in.read();
 			if (b != -1) {
 				length = offset;
-				process_command(input, length);
+				//process_command(input, length);
 				offset = 0;
 			}
 			/*load balance here*/
