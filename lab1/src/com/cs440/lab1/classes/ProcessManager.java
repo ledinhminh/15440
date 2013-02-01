022ff190871566aa5338dff9e8cf72cfcf6b8750
Kevin-Bravo <bravo@cmu.edu>
2013-02-01 03:19:28 -0500

    added some socket communication stuff

--- lab1/src/com/cs440/lab1/classes/ProcessManager.java
+++ lab1/src/com/cs440/lab1/classes/ProcessManager.java
@@ -1,6 +1,7 @@
 package com.cs440.lab1.classes;
 
 import java.util.*;
+import java.nio.channels.*;
 import java.io.BufferedReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -11,42 +12,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.net.*;
+//import com.cs440.lab1.interfaces.MigratableProcess;
 
-import com.cs440.lab1.interfaces.MigratableProcess;
-
-class ProcessTable {
-	/*holds a slave's process, and that process's time on this processor*/
+/*class ProcessTable {
 	private long time = 0;
 	String hostname;
-}
+}*/
 
+
+
+
+/* SlaveHost contains methods for load balancing, most importantly
+   a list of processes that its host currently holds
+ * the process list is FIFO, so processes that have been on this machine
+   the longest will be packaged and mailed somewhere
+*/
 class SlaveHost {
-	/*contains a slave to run on, and a process*/
-	private String hostname;
-	/*probably a better way to do this...
-	  hash currently used process id's so
-	  we can efficiently create new processes
-	*/
+	private SocketAddress slave_sock;
+	private int process_count;
 	private List<Integer> processId;
+
+	SlaveHost(SocketAddress _sock) {
+		slave_sock    = _sock;
+		process_count = 0;
+	}
+
 	public String getHostName() {
 		return hostname;
+	}
+	public Integer popProcess() {
+		if (processId.length == 0) {
+			System.err.println("popProcess: No Processes Remain.  Can't pop");
+			throw new Exception();
+		}
+		return processId.remove(0);
 	}
 }
 
 public class ProcessManager {
 	//The port for all the servers to run on
+	//do we need dis?  we have AFS
 	private static final int port = 15440;
-	int currentProcessId = 0;
+	private static final String OBJECT_DIR = "/afs/ blah blah kbravo / 440";
+	private int currentProcessId = 0;
+	private ServerSocket master_sock;
 	//list of slaves that are connected
-	List<SlaveHost> slave_list;
+	private List<SlaveHost> slave_list;
 	//mapping from processID to slave it is on.
-	Map<Integer, SlaveHost> processList;
-	boolean master;
-	ProcessServer server;
+	private Map<Integer, SlaveHost> processList;
+	private boolean master;
+	//private ProcessServer server;
 
 	public ProcessManager(boolean _isMaster, String masterUrl) {	
 		this.master = _isMaster;
-		server = new ProcessServer(port, this);
+		//server = new ProcessServer(port, this);
 	}
 	
 	/**
@@ -66,6 +86,7 @@ public class ProcessManager {
 	 * @param processId The id of the process to send
 	 */
 	private void sendProcessToSlave(int slaveId, int processId) {
+		
 		return;
 	}
 	
@@ -129,16 +150,29 @@ public class ProcessManager {
 		}
 	}
 	
-	public void startSlave() {}
+	public void startSlave() {
+	}
 	
 	public void startMaster() {
 		//setup the input stream reader
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		//open the master socket to communicate with slaves
+		master_sock           = new ServerSocket();
+		//master_sock.configureBlocking(false);
+		
 		while(true) {
 			//monitor stdin for new commands
 			
 			String input;
 			String[] args;
+			Socket slave_sock;
+
+			if ((slave_sock = master_sock.accept()) != null) {
+				//create a new slave host and store the remote socket address
+				SlaveHost slave_host = new SlaveHost(slave_sock.getRemoteSocketAddress());
+				System.out.println("Connected to " + slave_sock.toString());
+			}
 			
 			try {
 				input = reader.readLine();
@@ -165,8 +199,11 @@ public class ProcessManager {
 			MigratableProcess newProcess;
 			try {
 				Class<MigratableProcess> processClass = (Class<MigratableProcess>) Class.forName(args[0]);
+				
 				Constructor<MigratableProcess> processConstructor = processClass.getConstructor(String[].class);
+				
 				newProcess = processConstructor.newInstance((Object[])args);
+			
 			} catch (ClassNotFoundException e) {
 				//Couldn't link find that class. stupid user.
 				System.out.println("Could not find class " + args[0]);
